03225dfdf0b013f3498fba4d09d8e36473201ed8
justin403502 <justin403502@naver.com>
2025-11-28 18:11:33 +0900

    컴포넌트는 lazy 하지 않도록

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React, { useEffect, useRef, useState, Suspense } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Heading1 } from '@/components/Text.tsx';
 import Wrapper from '@/components/Wrapper.tsx';
-// import Account from '@/layout/Account/Account.tsx'; // Lazy loaded
+import Account from '@/layout/Account/Account.tsx';
 import Container from '@/layout/Container.tsx';
 import FloatingBar from '@/layout/FloatingBar/FloatingBar.tsx';
-// import PhotoGallery from '@/layout/Gallery/PhotoGallery.tsx'; // Lazy loaded
-// import BookGallery from '@/layout/BookGallery/BookGallery.tsx'; // Lazy loaded
-// import Guestbook from '@/layout/Guestbook/Guestbook.tsx'; // Lazy loaded
-// import Invitation from '@/layout/Invitation/Invitation.tsx'; // Lazy loaded
-// import Location from '@/layout/Location/Location.tsx'; // Lazy loaded
-// import Main from '@/layout/Main/Main.tsx'; // Lazy loaded
-
-const Main = React.lazy(() => import('@/layout/Main/Main.tsx'));
-const Invitation = React.lazy(() => import('@/layout/Invitation/Invitation.tsx'));
-const PhotoGallery = React.lazy(() => import('@/layout/Gallery/PhotoGallery.tsx'));
-const BookGallery = React.lazy(() => import('@/layout/BookGallery/BookGallery.tsx'));
-const Location = React.lazy(() => import('@/layout/Location/Location.tsx'));
-const Account = React.lazy(() => import('@/layout/Account/Account.tsx'));
-const Guestbook = React.lazy(() => import('@/layout/Guestbook/Guestbook.tsx'));
+import PhotoGallery from '@/layout/Gallery/PhotoGallery.tsx';
+import BookGallery from '@/layout/BookGallery/BookGallery.tsx';
+import Guestbook from '@/layout/Guestbook/Guestbook.tsx';
+import Invitation from '@/layout/Invitation/Invitation.tsx';
+import Location from '@/layout/Location/Location.tsx';
+import Main from '@/layout/Main/Main.tsx';
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
@@ -46,45 +38,31 @@ function App() {
   return (
     <Container>
       <Wrapper>
-        <Suspense fallback={<div>Loading Main...</div>}>
-          <Main />
-        </Suspense>
+        <Main />
       </Wrapper>
       <Wrapper>
         <Heading1>모시는 글</Heading1>
-        <Suspense fallback={<div>Loading Invitation...</div>}>
-          <Invitation />
-        </Suspense>
+        <Invitation />
       </Wrapper>
       <Wrapper ref={galleryRef}>
         <Heading1>Gallery</Heading1>
-        <Suspense fallback={<div>Loading PhotoGallery...</div>}>
-          <PhotoGallery />
-        </Suspense>
+        <PhotoGallery />
       </Wrapper>
       <Wrapper>
         <Heading1>Our Story</Heading1>
-        <Suspense fallback={<div>Loading BookGallery...</div>}>
-          <BookGallery />
-        </Suspense>
+        <BookGallery />
       </Wrapper>
       <Wrapper>
         <Heading1>오시는 길</Heading1>
-        <Suspense fallback={<div>Loading Location...</div>}>
-          <Location />
-        </Suspense>
+        <Location />
       </Wrapper>
       <Wrapper>
         <Heading1>마음 전하실 곳</Heading1>
-        <Suspense fallback={<div>Loading Account...</div>}>
-          <Account />
-        </Suspense>
+        <Account />
       </Wrapper>
       <Wrapper>
         <Heading1>신랑 신부에게</Heading1>
-        <Suspense fallback={<div>Loading Guestbook...</div>}>
-          <Guestbook />
-        </Suspense>
+        <Guestbook />
       </Wrapper>
       <FloatingBar isVisible={isVisible} />
     </Container>
